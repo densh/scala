ad068a028dd70aac5550365876f2a3b264ce3c98
Eugene Burmako <xeno.by@gmail.com>
2012-12-16 00:35:06 +0100

    q"def foo($x)"
    crashes, but it's not the parser's fault

--- src/compiler/scala/tools/reflect/quasiquotes/Parser.scala
+++ src/compiler/scala/tools/reflect/quasiquotes/Parser.scala
@@ -12,13 +12,15 @@ abstract class Parser extends ScalaParser {
   def parse(code: String): Tree = {
     val wrappedCode = "object wrapper {" + EOL + code + EOL + "}"
     val file = new BatchSourceFile("<quasiquotes>", wrappedCode)
-    val wrappedTree = new CustomSourceParser(file).parse()
+    val wrappedTree = new QuasiQuoteParser(file).parse()
     val PackageDef(_, List(ModuleDef(_, _, Template(_, _, _ :: parsed)))) = wrappedTree
-    (parsed match {
+    parsed match {
       case tree :: Nil => tree
       case stats :+ tree => Block(stats, tree)
-    })
+    }
   }
 
-  class CustomSourceParser(source0: SourceFile) extends SourceFileParser(source0)
+  class QuasiQuoteParser(source0: SourceFile) extends SourceFileParser(source0) {
+    override def allowTypelessParams = true
+  }
 }
