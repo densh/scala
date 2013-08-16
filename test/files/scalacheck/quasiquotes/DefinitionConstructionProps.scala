aadda82b0eed6192b4b69aba1c47353493a91f4c
Den Shabalin <den.shabalin@gmail.com>
2013-08-16 17:45:36 +0200

    a few more tests

--- test/files/scalacheck/quasiquotes/DefinitionConstructionProps.scala
+++ test/files/scalacheck/quasiquotes/DefinitionConstructionProps.scala
@@ -13,11 +13,6 @@ object DefinitionConstructionProps extends QuasiquoteProperties("errors") {
     assert(q"trait $Foo" ≈ q"trait Foo")
   }
 
-  // property("splice early valdef into trait") = test {
-  //   val x = q"val x: Int = 1"
-  //   assert(q"trait T extends { val $x } with Any" ≈ q"trait T extends { val x: Int = 1} with Any")
-  // }
-
   property("splice type params into trait def") = test {
     val tparams = q"type A" :: q"type B" :: Nil
     assert(q"trait Foo[..$tparams]" ≈ q"trait Foo[A, B]")
@@ -26,5 +21,15 @@ object DefinitionConstructionProps extends QuasiquoteProperties("errors") {
   property("splice defs into trait body") = test {
     val body = q"def foo" :: q"val bar: Baz" :: Nil
     assert(q"trait Foo { ..$body }" ≈ q"trait Foo { def foo; val bar: Baz }")
+  }
+
+  property("splice parents into trait") = test {
+    val parents = tq"A" :: tq"B" :: Nil
+    assert(q"trait Foo extends ..$parents" ≈ q"trait Foo extends A with B")
+  }
+
+  property("splice early valdef into trait") = test {
+    val x = q"val x: Int = 1"
+    assert(q"trait T extends { val $x } with Any" ≈ q"trait T extends { val x: Int = 1} with Any")
   }
 }
